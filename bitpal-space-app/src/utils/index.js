258fc70166e2912b50f9cd48f4070dc30474396d
Quốc Nguyên <61508148+QuoccNguyenn@users.noreply.github.com>
2021-05-19 17:36:58 +0700

    Delete bitpal-space-app directory

--- bitpal-space-app/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const getParam = (name) => {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-     if (results==null) {
-        return null;
-     }
-     return decodeURI(results[1]) || 0;
-   }
